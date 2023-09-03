f7f47fb12a874d49066b912c3c42671a712ae0c2
LakshayGarg18 <118999449+LakshayGarg18@users.noreply.github.com>
2023-09-03 18:18:08 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,159 @@
+import pyaudio
+import os
+import wave
+import librosa
+import numpy as np
+from sys import byteorder
+from array import array
+from struct import pack
+
+
+THRESHOLD = 500
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+RATE = 16000
+
+SILENCE = 30
+
+def is_silent(snd_data):
+    "Returns 'True' if below the 'silent' threshold"
+    return max(snd_data) < THRESHOLD
+
+def normalize(snd_data):
+    MAXIMUM = 16384
+    times = float(MAXIMUM)/max(abs(i) for i in snd_data)
+
+    r = array('h')
+    for i in snd_data:
+        r.append(int(i*times))
+    return r
+
+def trim(snd_data):
+
+    def _trim(snd_data):
+        snd_started = False
+        r = array('h')
+
+        for i in snd_data:
+            if not snd_started and abs(i)>THRESHOLD:
+                snd_started = True
+                r.append(i)
+
+            elif snd_started:
+                r.append(i)
+        return r
+
+    
+    snd_data = _trim(snd_data)
+
+    snd_data.reverse()
+    snd_data = _trim(snd_data)
+    snd_data.reverse()
+    return snd_data
+
+def add_silence(snd_data, seconds):
+    r = array('h', [0 for i in range(int(seconds*RATE))])
+    r.extend(snd_data)
+    r.extend([0 for i in range(int(seconds*RATE))])
+    return r
+
+def record():
+    
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT, channels=1, rate=RATE,
+        input=True, output=True,
+        frames_per_buffer=CHUNK_SIZE)
+
+    num_silent = 0
+    snd_started = False
+
+    r = array('h')
+
+    while 1:
+        snd_data = array('h', stream.read(CHUNK_SIZE))
+        if byteorder == 'big':
+            snd_data.byteswap()
+        r.extend(snd_data)
+
+        silent = is_silent(snd_data)
+
+        if silent and snd_started:
+            num_silent += 1
+        elif not silent and not snd_started:
+            snd_started = True
+
+        if snd_started and num_silent > SILENCE:
+            break
+
+    sample_width = p.get_sample_size(FORMAT)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    r = normalize(r)
+    r = trim(r)
+    r = add_silence(r, 0.5)
+    return sample_width, r
+
+def record_to_file(path):
+    "Records from the microphone and outputs the resulting data to 'path'"
+    sample_width, data = record()
+    data = pack('<' + ('h'*len(data)), *data)
+
+    wf = wave.open(path, 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(sample_width)
+    wf.setframerate(RATE)
+    wf.writeframes(data)
+    wf.close()
+
+
+
+def extract_feature(file_name, **kwargs):
+    mfcc = kwargs.get("mfcc")
+    chroma = kwargs.get("chroma")
+    mel = kwargs.get("mel")
+    contrast = kwargs.get("contrast")
+    tonnetz = kwargs.get("tonnetz")
+    X, sample_rate = librosa.core.load(file_name)
+    if chroma or contrast:
+        stft = np.abs(librosa.stft(X))
+    result = np.array([])
+    if mfcc:
+        mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+        result = np.hstack((result, mfccs))
+    if chroma:
+        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+        result = np.hstack((result, chroma))
+    if mel:
+        mel = np.mean(librosa.feature.melspectrogram(y=X, sr=sample_rate).T,axis=0)
+        result = np.hstack((result, mel))
+    if contrast:
+        contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+        result = np.hstack((result, contrast))
+    if tonnetz:
+        tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+        result = np.hstack((result, tonnetz))
+    return result
+
+
+if __name__ == "__main__":
+    from utils import load_data, split_data, create_model
+    import argparse
+    parser = argparse.ArgumentParser(description="""Gender recognition script, this will load the model you trained, 
+                                    and perform inference on a sample you provide (either using your voice or a file)""")
+    parser.add_argument("-f", "--file", help="The path to the file, preferred to be in WAV format")
+    args = parser.parse_args()
+    file = args.file
+    model = create_model()
+    model.load_weights("results/model.h5")
+    if not file or not os.path.isfile(file):
+        print("Please talk")
+        file = "test.wav"
+        record_to_file(file)
+    features = extract_feature(file, mel=True).reshape(1, -1)
+    male_prob = model.predict(features)[0][0]
+    female_prob = 1 - male_prob
+    gender = "male" if male_prob > female_prob else "female"
+    print("Result:", gender)
+    print(f"Probabilities:     Male: {male_prob*100:.2f}%    Female: {female_prob*100:.2f}%")
---
+++ utils.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import os
+import tqdm
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+from sklearn.model_selection import train_test_split
+
+
+label2int = {
+    "male": 1,
+    "female": 0
+}
+
+
+def load_data(vector_length=128):
+    if not os.path.isdir("results"):
+        os.mkdir("results")
+    if os.path.isfile("results/features.npy") and os.path.isfile("results/labels.npy"):
+        X = np.load("results/features.npy")
+        y = np.load("results/labels.npy")
+        return X, y
+    df = pd.read_csv("balanced-all.csv")
+    n_samples = len(df)
+    n_male_samples = len(df[df['gender'] == 'male'])
+    n_female_samples = len(df[df['gender'] == 'female'])
+    print("Total samples:", n_samples)
+    print("Total male samples:", n_male_samples)
+    print("Total female samples:", n_female_samples)
+    X = np.zeros((n_samples, vector_length))
+    y = np.zeros((n_samples, 1))
+    for i, (filename, gender) in tqdm.tqdm(enumerate(zip(df['filename'], df['gender'])), "Loading data", total=n_samples):
+        features = np.load(filename)
+        X[i] = features
+        y[i] = label2int[gender]
+    np.save("results/features", X)
+    np.save("results/labels", y)
+    return X, y
+
+
+def split_data(X, y, test_size=0.1, valid_size=0.1):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=7)
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=valid_size, random_state=7)
+    return {
+        "X_train": X_train,
+        "X_valid": X_valid,
+        "X_test": X_test,
+        "y_train": y_train,
+        "y_valid": y_valid,
+        "y_test": y_test
+    }
+
+
+def create_model(vector_length=128):
+    model = Sequential()
+    model.add(Dense(256, input_shape=(vector_length,)))
+    model.add(Dropout(0.3))
+    model.add(Dense(256, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(128, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(128, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(1, activation="sigmoid"))
+    model.compile(loss="binary_crossentropy", metrics=["accuracy"], optimizer="adam")
+    model.summary()
+    return model
